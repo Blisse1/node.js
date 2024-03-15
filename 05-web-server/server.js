0577564e1f2a5d8caa12a2ebd6632a45f2c71d4f
bliss <107047190+Blisse1@users.noreply.github.com>
2024-03-15 14:32:18 -0500

    Finish web server with Node

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -9,12 +9,35 @@ class Emitter extends EventEmitter { };
 
 // initialize object
 const myEmitter = new Emitter();
-
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
 const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+    try{
+        const rawData = await fsPromises.readFile(
+            filePath,
+            !contentType.includes("image") ? "utf8" : ""
+        );
+        const data = contentType === "application/json" 
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes("404.html") ? 404 : 200,
+            { "Content-Type": contentType });
+        response.end(
+            contentType === "application/json" ? JSON.stringify(data) : data
+        );
+    }catch(err){
+        console.log(err);
+        myEmitter.emit("log", `${err.name}\t${err.message}`, "reqLog.txt");
+        response.statusCode = 500;
+        response.end();
+    }
+
+}
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
-
+    myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
     const extension = path.extname(req.url);
 
     let contentType;
@@ -30,16 +53,16 @@ const server = http.createServer((req, res) => {
             contentType = "text/json";
             break;
         case ".jpg":
-            contentType = "text/jpg";
+            contentType = "image/jpeg";
             break;
         case ".png":
-            contentType = "text/png";
+            contentType = "image/png";
             break;
         case ".txt":
-            contentType = "text/.txt";
+            contentType = "text/txt";
             break;
         default:
-            contentType = "text/.html";
+            contentType = "text/html";
     }
 
     let filePath = 
@@ -57,6 +80,7 @@ const server = http.createServer((req, res) => {
     const fileExists = fs.existsSync(filePath);
     if(fileExists){
         // serve it
+        serveFile(filePath, contentType, res);
     }else{
         // 404 || 301 (redirect)
         switch(path.parse(filePath).base){
@@ -69,12 +93,11 @@ const server = http.createServer((req, res) => {
                 res.end();
                 break;
             default:
-                res.writeHead(301, {"Location": "/"});
-                res.end();
-                break;
+                serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
         }
     }
 
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
